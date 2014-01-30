834deb20e460372d23f97a3ee888f664b3c4356a
dianwen <dianwen@gmail.com>
2014-01-30 00:58:35 -0500

    Added to the XML parser
    The XML parser can now read "mass" tags and print them.

--- src/DOMParser.java
+++ src/DOMParser.java
@@ -1,9 +1,58 @@
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class DOMParser {
 
-	public DOMParser() {
-		// TODO Auto-generated constructor stub
+	public static void main(String args[]) {
+		try {
+
+			File stocks = new File("assets/Model_1.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(stocks);
+			doc.getDocumentElement().normalize();
+
+			System.out.println("root of xml file" + doc.getDocumentElement().getNodeName());
+			System.out.println("==========================");
+
+			//parse nodes
+			NodeList nodes = doc.getElementsByTagName("nodes");
+			for (int i = 0; i < nodes.getLength(); i++) {
+				NodeList nodeNodes = doc.getElementsByTagName("mass");
+				for( int j = 0; j < nodeNodes.getLength(); j++){
+					Node node = nodeNodes.item(j);
+					System.out.println("id: " + getNodeAttr("id", node) + " x: " + getNodeAttr("x", node) + " y: " + getNodeAttr("y", node) +
+									   " xvel: " + getNodeAttr("xvel", node) + " yvel: " + getNodeAttr("yvel", node) + " mass: " + getNodeAttr("mass", node));
+				}
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
-	
+
+	private static String getValue(String tag, Element element) {
+		NodeList nodes = element.getElementsByTagName(tag).item(0).getChildNodes();
+		Node node = (Node) nodes.item(0);
+		return node.getNodeValue();
+	}
+
+	protected static String getNodeAttr(String attrName, Node node ) {
+		NamedNodeMap attrs = node.getAttributes();
+		for (int y = 0; y < attrs.getLength(); y++ ) {
+			Node attr = attrs.item(y);
+			if (attr.getNodeName().equalsIgnoreCase(attrName)) {
+				return attr.getNodeValue();
+			}
+		}
+		return "";
+	}
 
 }
