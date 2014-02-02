b26d5cc3aedb966b3f0ea2ac23c03dac4ca3b602
dianwen <dianwen@gmail.com>
2014-02-02 16:06:04 -0500

    Refactored

--- src/springies/Springies.java
+++ src/springies/Springies.java
@@ -17,7 +17,6 @@ import nodes.Mass;
 import nodes.SuperMass;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
