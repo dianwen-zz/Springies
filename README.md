ead5103402c4a30ef33d905df4d0178230f37ea0
dianwen <dianwen@gmail.com>
2014-02-04 16:50:41 -0500

    Deleted error logs, updated README

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-springies
+Springies
 =========
 
-Starter code for Springies team project
+Team Members: Dianwen Li & Anthony Olawo
+Date started: Jan 30th
 
 
+This project is a mass/spring/environmental force simulator using the JBox2D physics engine and the JGame game engine.
+
+Load an XML file to draw and animate an assembly. Press 'n' in the simulation to load another file, and press 'c' to clear all assemblies.
--- src/nodes/SuperMass.java
+++ src/nodes/SuperMass.java
@@ -11,33 +11,33 @@ import jgame.JGColor;
 public abstract class SuperMass extends PhysicalObject{
 	protected float mass;
 	private static final int RADIUS = 5;
-	
+
 	public SuperMass(String id, int COLLISION_ID, JGColor color, float xpos, float ypos, float m) {
 		// TODO Auto-generated constructor stub	
 		super(id, COLLISION_ID, color);		
 		init(xpos,ypos,m,0);
 	}
-	
+
 	void init(float xpos, float ypos, float m, float g){
 		CircleDef shape = new CircleDef();
 		shape.radius = (float) RADIUS;
 		shape.density = (float) m;
-		
+
 		createBody(shape);
-        setBBox(-(int)RADIUS, -(int)RADIUS, 2 * (int)RADIUS, 2 * (int)RADIUS);
-		
+		setBBox(-(int)RADIUS, -(int)RADIUS, 2 * (int)RADIUS, 2 * (int)RADIUS);
+
 		this.setPos(xpos,ypos);
 		this.setForce(0, 0);
 	}
-	
-    @Override
-    public void paintShape ()
-    {
-        myEngine.setColor(myColor);
-        myEngine.drawOval(x, y, (float)RADIUS * 2, (float)RADIUS * 2, true, true);
-    }
-    
-    public float getMass(){
-    	return mass;
-    }
+
+	@Override
+	public void paintShape ()
+	{
+		myEngine.setColor(myColor);
+		myEngine.drawOval(x, y, (float)RADIUS * 2, (float)RADIUS * 2, true, true);
+	}
+
+	public float getMass(){
+		return mass;
+	}
 }
